49b09ad6cd0fc0d19da1c4a2861d88b0c1f0097a
KatyPreston <prestonkj@hotmail.com>
2018-04-29 13:00:33 +0100

    mvp completed

---
+++ CodeClan Cinema/models/customer.rb
@@ -0,0 +1,74 @@
+require_relative("../db/sql_runner")
+
+class Customer
+
+  attr_reader :id
+  attr_accessor :name, :funds
+
+  def initialize( options )
+    @id = options["id"].to_i if options["id"]
+    @name = options["name"]
+    @funds = options["funds"].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO customers
+    (
+      name,
+      funds
+    )
+    VALUES
+    (
+      $1, $2
+    )
+    RETURNING id"
+    values = [@name, @funds]
+    customer = SqlRunner.run( sql, values )[0]
+    @id = customer["id"].to_i
+  end
+
+
+  def update()
+    sql = "UPDATE customers SET name = $1 WHERE id = $2"
+    values = [@name, @id]
+    SqlRunner.run( sql, values )
+  end
+
+
+  def delete()
+    sql = "DELETE FROM customers WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def films()
+    sql = "SELECT films.*
+    FROM films
+    INNER JOIN tickets
+    ON tickets.film_id = films.id
+    WHERE customer_id = $1"
+    values = [@id]
+    film_data = SqlRunner.run(sql, values)
+    return Film.map_items(film_data)
+  end
+
+
+  def self.map_items(customer_data)
+    result = customer_data.map { |customer| Customer.new(customer) }
+    return result
+  end
+
+
+  def self.all()
+      sql = "SELECT * FROM customers"
+      customer_data = SqlRunner.run(sql)
+      return Customer.map_items(customer_data)
+    end
+
+
+  def self.delete_all()
+   sql = "DELETE FROM customers"
+   SqlRunner.run(sql)
+  end
+
+end
